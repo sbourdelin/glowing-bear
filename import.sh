52d3c32bdd1d6a723964cfcfde6ecbede4cdf66f
Vivien Didelot <vivien.didelot@savoirfairelinux.com>
2013-08-05 16:34:09 -0400

    inject version in the html page

--- import.sh
+++ import.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-git checkout master -- .
-git commit -am "import $(git rev-parse --short master)"
+REV=$(git rev-parse --short master)
+
+echo "Checking out sources..."
+git checkout $REV -- .
+
+echo "Updating version..."
+sed -i "/<\/body>/i\
+    <p>Version <a href=\"https:\/\/github.com\/cormier\/glowing-bear\/commit\/$REV\">$REV<\/a><\\p>
+
+" index.html
+
+echo "Committing..."
+git commit -am "import $REV"
